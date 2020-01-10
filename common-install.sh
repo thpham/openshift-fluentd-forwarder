3cf25eeda70b9e2033996e37401b8b055d68f838
J.R. Morgan <liveaverage@users.noreply.github.com>
2020-01-10 10:29:50 -0500

    Use rh-ruby24

--- common-install.sh
+++ common-install.sh
@@ -13,7 +13,7 @@ YUM_ARGS="--setopt=tsflags=nodocs"
 PACKAGES="gem gcc-c++ libcurl-devel make bc gettext nss_wrapper hostname iproute"
 
 # ruby packages
-PACKAGES="${PACKAGES} rh-ruby22 rh-ruby22-rubygems rh-ruby22-ruby-devel"
+PACKAGES="${PACKAGES} rh-ruby24 rh-ruby24-rubygems rh-ruby24-ruby-devel"
 
 # if the release is a red hat version then we need to set additional arguments for yum repositories
 RED_HAT_MATCH='^Red Hat.*$'
